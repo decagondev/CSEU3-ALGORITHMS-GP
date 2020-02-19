ca88b71a14b6636ccb2912d4b15480bc35d105c4
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-02-19 11:39:09 +0100

    big o

--- challenge.py
+++ challenge.py
@@ -1,29 +1,28 @@
 
 # Challenge What is the runtime complexity of this function
 
-def power_r(a, b):
-    # Error checking
+def power_r(a,b):
+    # Error cehcking
     # try to cast our exponent to an int
     try:
-        _ = int(b)
+        _ = int(b) # O(1)
     # exception on fail with error message
     except ValueError:
-        print("Exponent (b) must be and integer")
-        # and return
-        return
-
+        print(f"Exponent (b) = ({b}) must be an integer") # O(1)
+        return # O(1)
+    # and return
     # base case
-    # anything raised to the power of 0 will be one
-    if b == 0:
-        return 1
-    # positive case if b is greater than zero
-    elif b > 0:   
+    if b == 0: # O(1)
+        return 1 # O(1)
+    # recursive positive case
+    elif b > 0: # O(1)
         # Recursive case
-        # Call the function on b - 1
-        return a * power_r(a, b - 1)
-    # Recursive negative exponent
+        # call the function on b - 1
+
+        return a * power_r(a, b - 1) # O(n) * O(1)
+    # recursive negative case
     else:
-        # return 1 divided by a multiplied by the function with -b - 1
-        return 1 / (a * power_r(a, -b - 1))
+        # return 1 divided by a multiplied by the function with b - 1
+        return 1 / (a * power_r(a, -b - 1)) # O(1) + O(n) * O(1)
         # or return 1 divided by function with -b
         # return 1 / power_r(a, -b)
