62c7b270bdf0e291b268ed705200128434be27cf
Tom <tomtarpeydev@gmail.com>
2020-02-20 09:55:19 +0000

    spec of anagram problem

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -1,1 +1,17 @@
 # Given a text file words.txt, print out the largest set of anagrams
+
+"""
+1. Understand:
+    What is an anagram?
+    - anagram: a word, phrase, or name formed by rearanging the letters 
+    of an other, such as spar, formed from rasp
+
+2. > we need to generate all anagram sets
+    - should have the same number of each letter
+    - ANAGRAMS should be equal to the original word when both are sorted
+    - How do we want to handle case? (ignore case)
+   > once all sets are generated, we need to find the largest set
+    - utilize some sort of MAX function
+
+3. Implement
+"""
